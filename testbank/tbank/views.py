7cc88edaa53d84d24b497f140b6c6d005a3c2b90
Ali Saadati <saadati944@gmail.com>
2020-12-28 18:57:28 +0330

    add index page

--- testbank/tbank/views.py
+++ testbank/tbank/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class index(TemplateView):
+    template_name="index"
+    def get(self, request, **kwargs):
+        return render(request, 'index.html')
