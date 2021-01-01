c38fe7ed9ae3387fbd768154edf5e657fcb3acd4
Ali Saadati <saadati944@gmail.com>
2021-01-01 13:58:01 +0330

    The database was returned to sqlite for simplicity

--- testbank/testbank/settings.py
+++ testbank/testbank/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = 'testbank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
-
+}
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
@@ -94,7 +94,7 @@ DATABASES = {
         }
     }
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
